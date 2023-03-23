0e623d00edb255acdab626a0f89701ce5447826f
Elijah <ekuradusenge2004@gmail.com>
2023-03-23 13:27:41 +0200

    web-designer portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React from "react";
+import emailjs from '@emailjs/browser';
+// motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div>Contact</div>;
+ 
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_5na9lr9', e.target, '3E7XVEblPgZrY0vVv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    };
+  return (
+    <section className="py-16 lg:section" id="contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text */}
+          <motion.div
+            className="flex-1"
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+          >
+            <div>
+              <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">
+                Get in Touch
+              </h4>
+              <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+                Let's work <br /> Together
+              </h2>
+            </div>
+          </motion.div>
+          {/* form */}
+          <motion.form
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            onSubmit={sendEmail}
+            className="flex-1 border rounded-2xl  flex flex-col  gap-y-6 pb-24 p-6 items-start"
+          >
+
+            <input
+              type="text"
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Your name"
+              name="name"
+            />
+            <input
+              type="text"
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Your name"
+              name="name"
+            />
+            <input
+              type="text"
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              placeholder="Your email"
+              name="email"
+            />
+            <textarea
+              className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white 
+          focus:border-accent transition-all resize-none mb-12"
+              placeholder="Your message"
+              name="message"
+            ></textarea>
+            <button className="btn btn-lg" type="submit">Send message</button>
+          </motion.form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
